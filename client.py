3276c381c0f91143af846194f44fbb165d77a6e8
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-09-29 00:11:46 +0100

    changed thread logic in order to fix the bug. when user disconnects, gets removed from the array list "clients" calculator is now functional, but will be changed tomorrow

--- client.py
+++ client.py
@@ -1,69 +1,11 @@
 import socket
-import threading
-
-def menu():
-    while True:
-        print('1 - Messenger')
-        print('2 - Calculator')
-        print('0 - Quit')
-        op1 = int(input('Choose a option:'))
-
-        if op1 == 1:
-            receive_thread = threading.Thread(target=receive_messages, args=(client_socket, name))
-            receive_thread.start()
-
-            while True:
-                message = input()
-                if message == "/return":
-                    break
-                client_socket.send(message.encode())
-
-        elif op1 == 2:
-            while True:
-                print('1 - Add')
-                print('2 - Sub')
-                print('3 - Div')
-                print('4 - Mul')
-                print('0 - Return')
-                op2 = int(input('Choose a option:'))
-
-                if op2 == 1:
-                    pass
-                elif op2 == 2:
-                    pass
-                elif op2 == 3:
-                    pass
-                elif op2 == 4:
-                    pass
-                elif op2 == 0:
-                    break
-                else:
-                    print('\nChoose a valid option!')
-
-        elif op1 == 0:
-            response = input('Are you sure you want to leave the application? (y/n)')
-            if response == 'y':
-                print('Leaving the program ...')
-                client_socket.close()
-                return  
-            elif response == 'n':
-                continue 
-
-        else:
-            print('\nChoose a valid option!')
-
-def receive_messages(client_socket, client_name):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            print(f"{message}")
-            print()
-        except:
-            break
+import os
 
 # Configurações do cliente
 host = socket.gethostname()
 port = 12345
+
+HEADER_LENGTH = 10
 
 ip = socket.gethostbyname(host)
 
@@ -74,4 +16,60 @@ client_socket.connect((ip, port))
 name = input('Introduce your name:')
 client_socket.send(name.encode())
 
-menu()
+while True:
+    print('1 - Messenger')
+    print('2 - Calculator')
+    print('0 - Quit')
+    op1 = int(input('Choose an option:'))
+
+    if op1 == 1:
+        while True:
+            message = input("Enter your message (type '/return' to go back to menu): ")
+            if message == "/return":
+                break
+            client_socket.send(message.encode())
+
+    elif op1 == 2:
+        n1 = float(input('Introduce the first number:'))
+        n2 = float(input('Introduce the second number:'))
+        
+        while True:
+            print('1 - Add')
+            print('2 - Sub')
+            print('3 - Div')
+            print('4 - Mul')
+            print('0 - Return')
+            op2 = int(input('Choose an option:'))
+
+            if op2 == 1:
+                value = n1 + n2
+                client_socket.send(str(value).encode())
+                break
+            elif op2 == 2:
+                value = n1 - n2
+                client_socket.send(str(value).encode())
+                print(f'O valor da conta: {value}')
+                break
+            elif op2 == 3:
+                value = n1 / n2
+                client_socket.send(str(value).encode())
+                break
+            elif op2 == 4:
+                value = n1 * n2
+                client_socket.send(str(value).encode())
+                break
+            elif op2 == 0:
+                break
+            else:
+                print('Choose a valid option!')
+
+    elif op1 == 0:
+        response = input('Are you sure you want to leave the application? (y/n)')
+        if response == 'y':
+            print('Leaving the program ...')
+            client_socket.close()
+            os.system('cls')
+            break
+
+    else:
+        print('Choose a valid option:')
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 import threading
 
+# Dicionário para rastrear os clientes conectados
 clients = {}
-disconnected_clients = {}  # Manter o registro de clientes desconectados
 
-# Função para lidar com as mensagens de um cliente
+# Função para lidar com as mensagens enviadas por um cliente
 def handle_client_input(client_socket, client_name):
     while True:
         try:
@@ -15,25 +15,21 @@ def handle_client_input(client_socket, client_name):
             full_message = f"{client_name}: {message}"
             print(f"{full_message}")
             
-            for other_client_socket, other_client_name in clients.items():
+            # Enviar a mensagem para todos os clientes conectados
+            for other_client_socket in clients.values():
                 if other_client_socket != client_socket:
                     other_client_socket.send(full_message.encode())
         except:
             break
-    
-    # Move o cliente para o registro de clientes desconectados quando a conexão é encerrada
-    del clients[client_socket]
-    disconnected_clients[client_socket] = client_name
-    client_socket.close()
 
-def handle_client_output(client_socket):
-    while True:
-        pass
+    # Quando o cliente desconecta, removê-lo da lista de clientes
+    del clients[client_name]
+    client_socket.close()
+    print(f"Connection ended with the client {client_address} --> Goodbye {client_name}")
 
 # Configurações do servidor
 host = socket.gethostname()
 port = 12345
-
 ip = socket.gethostbyname(host)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,20 +41,16 @@ server_socket.listen(5)
 print(f"Server Alocated on IP: {ip} with the door: {port}")
 
 while True:
-    # Aceita uma conexão quando um cliente se conecta
+    # Código para aceitar uma conexão quando o cliente se conecta
     client_socket, client_address = server_socket.accept()
     client_name = client_socket.recv(1024).decode()
     
+    client_address = f"{ip}:{port}"
+
     print(f"Connection established with the client {client_address} --> Welcome {client_name}")
     
-    if client_socket in disconnected_clients:
-        clients[client_socket] = disconnected_clients[client_socket]
-        del disconnected_clients[client_socket]
-    
-    clients[client_socket] = client_name
-    
-    input_thread = threading.Thread(target=handle_client_input, args=(client_socket, client_name))
-    input_thread.start()
-    
-    output_thread = threading.Thread(target=handle_client_output, args=(client_socket,))
-    output_thread.start()
+    clients[client_name] = client_socket  # Adicione o cliente à lista de clientes
+
+    # Inicie uma nova thread para lidar com as mensagens do cliente
+    client_thread = threading.Thread(target=handle_client_input, args=(client_socket, client_name))
+    client_thread.start()
