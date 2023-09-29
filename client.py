7c126687b51f245ce3c505097e46de7f92a18347
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-09-29 10:33:58 +0100

    fixed calculator fixed the difference between a "calculator" message on server.py cmd and a "normal" message on server.py cmd. new codification / descodification made create functions to clean code

--- client.py
+++ client.py
@@ -1,75 +1,63 @@
 import socket
 import os
 
+def menu():
+    while True:
+        print('1 - Messenger')
+        print('2 - Calculator')
+        print('0 - Quit')
+        op1 = int(input('Choose an option:'))
+
+        if op1 == 1:
+            while True:
+                # Aqui, aparece um input para o client introduzir uma mensagem quando escolhe a opção 1 do menu inicial que lhe aparece quando se autentica ao server.
+                # Caso escrever uma mensagem, esta é codificada usando o algoritmo "utf-8" e é enviada para o server, onde será transmitida no cmd
+                # Caso escreva "/return", dá break e envia o cliente de volta para o menu inicial.
+                message = input("Enter your message (type '/return' to go back to menu): ")
+                if message == "/return":
+                    break
+                client_socket.send(message.encode("utf-8"))
+
+        elif op1 == 2:
+            operation = input('Introduce your mathematic operation:')
+            
+            # Tenta realizar a operação matematica, caso sejam introduzidos letras ou a sintaxe esteja errada, é mostrado uma exception na linha de comandos do cliente
+            # De seguida, passamos a variável "res" para string, de modo a evitarmos erros na codificação e envio da "mensagem"
+            try:
+                res = eval(operation)
+                res_str = str(res)
+                client_socket.send(f"calc_result: {res_str}".encode("utf-8"))
+                print(f'The final result of the calculation is: {res_str}')
+            except Exception as e:
+                print(f"An Error has appeared: {e}")
+
+        elif op1 == 0:
+
+            # Quando o cliente escolhe a ultima opção, é perguntado se deseja mesmo sair do programa e caso escolha "y", o socket da close à ligação entre o cliente e o servidor e a consola é limpa, usando a biblioteca os.
+            response = input('Are you sure you want to leave the application? (y/n):')
+            if response == 'y':
+                print('Leaving the program ...')
+                client_socket.close()
+                os.system('cls')
+                break
+
+        else:
+            print('Choose a valid option:')
+
 # Configurações do cliente
 host = socket.gethostname()
 port = 12345
-
-HEADER_LENGTH = 10
-
 ip = socket.gethostbyname(host)
+
+# Limitar o números de bits (usado na calculadora)
+HEADER_LENGTH = 10
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((ip, port))
 
+# Introdução do nome do cliente
 name = input('Introduce your name:')
-client_socket.send(name.encode())
+client_socket.send(name.encode("utf-8"))
 
-while True:
-    print('1 - Messenger')
-    print('2 - Calculator')
-    print('0 - Quit')
-    op1 = int(input('Choose an option:'))
-
-    if op1 == 1:
-        while True:
-            message = input("Enter your message (type '/return' to go back to menu): ")
-            if message == "/return":
-                break
-            client_socket.send(message.encode())
-
-    elif op1 == 2:
-        n1 = float(input('Introduce the first number:'))
-        n2 = float(input('Introduce the second number:'))
-        
-        while True:
-            print('1 - Add')
-            print('2 - Sub')
-            print('3 - Div')
-            print('4 - Mul')
-            print('0 - Return')
-            op2 = int(input('Choose an option:'))
-
-            if op2 == 1:
-                value = n1 + n2
-                client_socket.send(str(value).encode())
-                break
-            elif op2 == 2:
-                value = n1 - n2
-                client_socket.send(str(value).encode())
-                print(f'O valor da conta: {value}')
-                break
-            elif op2 == 3:
-                value = n1 / n2
-                client_socket.send(str(value).encode())
-                break
-            elif op2 == 4:
-                value = n1 * n2
-                client_socket.send(str(value).encode())
-                break
-            elif op2 == 0:
-                break
-            else:
-                print('Choose a valid option!')
-
-    elif op1 == 0:
-        response = input('Are you sure you want to leave the application? (y/n)')
-        if response == 'y':
-            print('Leaving the program ...')
-            client_socket.close()
-            os.system('cls')
-            break
-
-    else:
-        print('Choose a valid option:')
+menu()
--- server.py
+++ server.py
@@ -5,52 +5,66 @@ import threading
 clients = {}
 
 # Função para lidar com as mensagens enviadas por um cliente
-def handle_client_input(client_socket, client_name):
+def handle_client_input(client_socket, client_name, client_address):
     while True:
         try:
-            message = client_socket.recv(1024).decode()
+            # Aqui, o server tenta receber a mensagem enviada pelo cliente e descodifica, mesmo que seja da opção 1 (Messenger) ou 2 (Calculadora)
+            # Se não for "true", volta para o menu
+            message = client_socket.recv(1024).decode("utf-8")
             if not message:
                 break
             
-            full_message = f"{client_name}: {message}"
-            print(f"{full_message}")
-            
+            # Neste caso, quando a mensagem começa "startswitch" com calc_result, é enviado um print a dizer que esta mensagem é proveniente da calculadora, e que cliente fez a operação matemática
+            if message.startswith("calc_result: "):
+                calc_result = message[len("calc_result: "):]
+                print(f"The value of the operation made by {client_name} is: {calc_result}")
+            else:
+
+                # Senão for mensagem de calculo, é enviado uma mensagem para o cmd, com o nome do cliente que a enviou e o conteúdo da mensagem
+                full_message = f"{client_name}: {message}"
+                print(f"{full_message}")
+
             # Enviar a mensagem para todos os clientes conectados
             for other_client_socket in clients.values():
                 if other_client_socket != client_socket:
-                    other_client_socket.send(full_message.encode())
-        except:
+                    other_client_socket.send(full_message.encode("utf-8"))
+        except Exception as e:
+            print(f"An Error has appeared: {e}")
             break
 
-    # Quando o cliente desconecta, removê-lo da lista de clientes
+    # Quando a conexão do cliente é terminada, o mesmo é removida da lista do clients
     del clients[client_name]
     client_socket.close()
     print(f"Connection ended with the client {client_address} --> Goodbye {client_name}")
+
+def handle_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind((ip, port))
+
+    server_socket.listen(5)
+
+    print(f"Server Alocated on IP: {ip} with the door: {port}")
+
+    while True:
+        # Código para aceitar uma conexão quando o cliente se conecta
+        client_socket, client_address = server_socket.accept()
+        client_name = client_socket.recv(1024).decode("utf-8")
+
+        client_address = f"{ip}:{port}"
+
+        print(f"Connection established with the client {client_address} --> Welcome {client_name}")
+
+        clients[client_name] = client_socket  # Adicione o cliente à lista de clientes
+        print(clients)
+
+        # Inicie uma nova thread para lidar com as mensagens do cliente
+        client_thread = threading.Thread(target=handle_client_input, args=(client_socket, client_name, client_address))
+        client_thread.start()
 
 # Configurações do servidor
 host = socket.gethostname()
 port = 12345
 ip = socket.gethostbyname(host)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_socket.bind((ip, port))
-
-server_socket.listen(5)
-
-print(f"Server Alocated on IP: {ip} with the door: {port}")
-
-while True:
-    # Código para aceitar uma conexão quando o cliente se conecta
-    client_socket, client_address = server_socket.accept()
-    client_name = client_socket.recv(1024).decode()
-    
-    client_address = f"{ip}:{port}"
-
-    print(f"Connection established with the client {client_address} --> Welcome {client_name}")
-    
-    clients[client_name] = client_socket  # Adicione o cliente à lista de clientes
-
-    # Inicie uma nova thread para lidar com as mensagens do cliente
-    client_thread = threading.Thread(target=handle_client_input, args=(client_socket, client_name))
-    client_thread.start()
+handle_server()
